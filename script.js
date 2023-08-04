8326ddfb92033f88e73d072106556d02cafa68ab
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-08-04 15:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const count = document.getElementById("counter");
+
+ let counter = 0;
+
+function butnfun(){
+ 
+    counter = counter+1;
+  alert(count.innerHTML=counter);
+
+}
